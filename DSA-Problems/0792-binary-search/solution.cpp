7a8d73df152853deb7540b4cd6cbb3276e44beaf
Abhinav Gupta <abhinavg01441@gmail.com>
2025-08-22 13:14:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.3 MB (80.33%)

---
+++ DSA-Problems/0792-binary-search/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int st = 0, end = n-1;
+
+        while(st<=end){
+            // int mid = (st+end)/2; // not good for large size of arrays and could lead to overflow.
+
+            // optimised value for mid
+            int mid = (st+ (end-st)/2);
+
+            if(target>nums[mid]){
+                st = mid+1;
+            }
+            else if(target<nums[mid]){
+                end = mid-1;
+
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+        
+    }
+};
