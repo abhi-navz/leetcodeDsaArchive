b2cab8a247d24769cfe053786245c35761b62086
Abhinav Gupta <abhinavg01441@gmail.com>
2025-09-09 06:30:15 +0000

    Sync LeetCode submission Runtime - 36 ms (80.56%), Memory - 48.4 MB (21.14%)

--- DSA-Problems/0560-subarray-sum-equals-k/solution.cpp
+++ DSA-Problems/0560-subarray-sum-equals-k/solution.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
 
-        int count = 0;
-       
-
+        int count =0;
+        long long sum = 0;
+        unordered_map<long long , int> preSumMap;
+        preSumMap[0] =1; // base case in case we got k at first. we'll get remaining as zero, so by this we use our preSumMap to know ki yeah the rem is present in hashmap. k-k =0
 
         for(int i =0; i<nums.size(); i++){
-            int currSum =0;
-            for(int j =i; j<nums.size(); j++){
-                currSum += nums[j];
+            sum += nums[i];
 
-                if( currSum == k){
-                    count++;
-                }
+            long long rem = sum -k;
+
+            if(preSumMap.find(rem) != preSumMap.end()){
+                count += preSumMap[rem]; // add all occurences.
             }
 
-            
+            preSumMap[sum]++;
 
         }
-        return count;
 
-        
+        return count;
         
     }
 };
