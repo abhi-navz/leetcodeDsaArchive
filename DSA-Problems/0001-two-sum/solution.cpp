607dfbae5ffd9f808c6f80a6cd095883552dd585
Abhinav Gupta <abhinavg01441@gmail.com>
2025-09-10 17:41:40 +0000

    Sync LeetCode submission Runtime - 2 ms (71.96%), Memory - 14.9 MB (36.44%)

--- DSA-Problems/0001-two-sum/solution.cpp
+++ DSA-Problems/0001-two-sum/solution.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size(); i++){
-            for(int j =i+1;j<nums.size(); j++){
-               if (nums[i] +nums[j] == target) {
-                return {i,j};
-               }
+        unordered_map<int, int> arrMap;
+        
+        
+
+        for(int i =0; i<nums.size();i++){
+            
+            int x = target - nums[i];
+
+            // checking for the x;
+            //Why we CHECK FIRST:
+            // If the complement (x) already exists in the map, it means we have seen         another number earlier that together with nums[i] adds up to target.
+            // We must check BEFORE inserting nums[i], because:If we insert first, the map will overwrite old indices for duplicate numbers (e.g., nums = [3,3], target=6). Then we would lose the original index and fail to return {0,1}
+
+            if(arrMap.find(x) != arrMap.end()){
+               
+               return {arrMap[x], i};
             }
+
+            // inserting into the map
+            arrMap[nums[i]] = i;    
+
         }
+
+        
+
         return {};
 
 
-        
-        
         
     }
 };
