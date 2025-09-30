99a5a0b3b2c654326787d7dbf39584065480f930
Abhinav Gupta <abhinavg01441@gmail.com>
2025-09-30 18:17:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.9 MB (81.00%)

---
+++ DSA-Problems/0349-intersection-of-two-arrays/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+        vector<int> ans;
+        int n = nums1.size();
+        int m = nums2.size();
+        int i =0 , j=0;
+
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        while(i<n && j<m){
+            if(nums1[i]< nums2[j]){
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else{
+                if(ans.empty() || nums1[i] != ans.back()){
+
+                    ans.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+
+
+
+        return ans;
+        
+    }
+};
