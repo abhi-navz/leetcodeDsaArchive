f1c8c22ffdd7f0218d3f8e4525d7e35e273270bf
Abhinav Gupta <abhinavg01441@gmail.com>
2025-10-09 18:19:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.3 MB (99.97%)

---
+++ DSA-Problems/0225-implement-stack-using-queues/solution.cpp
@@ -0,0 +1,49 @@
+class MyStack {
+    queue<int> q;
+public:
+    MyStack() {
+
+        
+    }
+    
+    void push(int x) {
+       
+        q.push(x);
+        for(int i =0; i<q.size()-1; i++){
+            int y = q.front();
+            q.pop();
+            q.push(y);
+        }
+        
+    }
+    
+    int pop() {
+        if(q.empty()){
+            return -1;
+        }
+        int y = q.front();
+        q.pop();
+        return y;
+        
+    }
+    
+    int top() {
+        if(q.empty()) return -1;
+        return q.front();
+        
+    }
+    
+    bool empty() {
+        return q.empty();
+        
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
