8465b184d47c4f279ed2be745009243cfbae191b
Abhinav Gupta <abhinavg01441@gmail.com>
2025-11-22 13:18:49 +0000

    Sync LeetCode submission Runtime - 412 ms (73.77%), Memory - 0.0B (100.00%)

--- DSA-Problems/0197-rising-temperature/solution.sql
+++ DSA-Problems/0197-rising-temperature/solution.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-select curr.id from weather curr join weather prev on 
-curr.recordDate = prev.recordDate + interval 1 Day
-where curr.temperature > prev.temperature;
+select curr.id 
+    from weather curr join weather prev
+    on  curr.recordDate = prev.recordDate + interval 1 Day
+    where curr.temperature > prev.temperature;
