942e67b21f7b4d52a7c641b2d5310c2a5bc1db35
Abhinav Gupta <abhinavg01441@gmail.com>
2025-11-22 15:04:23 +0000

    Sync LeetCode submission Runtime - 321 ms (72.42%), Memory - 0.0B (100.00%)

---
+++ DSA-Problems/2127-employees-whose-manager-left-the-company/solution.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+select 
+    e.employee_id 
+from employees e
+where 
+    e.salary < 30000
+    and e.manager_id is not null
+    and not exists(
+    select 1 
+    from employees m
+    where m.employee_id = e.manager_id
+    )
+order by e.employee_id;    
+   
