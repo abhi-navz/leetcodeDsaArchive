1539dfc6e31715457f252c83b7d08982a6f647f5
Abhinav Gupta <abhinavg01441@gmail.com>
2026-01-17 19:53:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.2 MB (36.29%)

---
+++ DSA-Problems/0812-rotate-string/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+
+        for( int i =0; i<s.size(); i++){
+
+            char ch = s[0];
+            s.erase(0,1);
+            s.push_back(ch);
+
+            if(s == goal) return true;
+
+        }
+
+        return false;
+        
+    }
+};
