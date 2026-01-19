9e7b7466603a5e7b02ec3f3dba9c8aa503f0770e
Abhinav Gupta <abhinavg01441@gmail.com>
2026-01-19 19:36:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.9 MB (77.24%)

--- DSA-Problems/0104-maximum-depth-of-binary-tree/solution.cpp
+++ DSA-Problems/0104-maximum-depth-of-binary-tree/solution.cpp
@@ -13,13 +13,14 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
 
-        // the intuiton is that the height of maximum depth of a bianry tree will be [1 + max of height of left subtree and right subtree]
 
-        // base case: if the root is nullptr , the height will be zero.
-        if(root == nullptr){
-            return 0;
-        }
-        return 1+ max(maxDepth(root->left), maxDepth(root->right));
+        // base case 
+        if(root == nullptr) return 0;
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+
+        return 1 + max(lh,rh);
+
         
     }
 };
