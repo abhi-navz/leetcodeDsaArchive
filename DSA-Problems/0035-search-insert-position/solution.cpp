9c1d7494e198fb33385399b2f7c57945f7734e95
Abhinav Gupta <abhinavg01441@gmail.com>
2026-02-04 03:56:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.7 MB (42.02%)

---
+++ DSA-Problems/0035-search-insert-position/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+
+        int low = 0;
+        int high =nums.size()-1;
+
+       int mid; 
+        while(low<=high){
+             mid = low +(high -low)/2;
+            if(nums[mid]<target){
+                low = mid+1;
+            }else if(nums[mid]>target){
+                high = mid-1;
+            }else{
+                return mid;
+            }
+
+        }
+       return low;
+        
+    }
+};
