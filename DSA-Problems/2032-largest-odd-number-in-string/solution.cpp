984b5f49a18cacb487a2a01f9d7b26c39b911d8b
Abhinav Gupta <abhinavg01441@gmail.com>
2026-02-05 08:06:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (65.12%)

--- DSA-Problems/2032-largest-odd-number-in-string/solution.cpp
+++ DSA-Problems/2032-largest-odd-number-in-string/solution.cpp
@@ -3,19 +3,17 @@ public:
     string largestOddNumber(string num) {
 
         int n = num.size();
-        int j = n - 1;
-
-        while (j >= 0) {
+        int length = 0;
+        for(int j=n-1; j>=0; j--){
             int digit = num[j] - '0';
-            if (digit % 2 == 1) {
+
+            if(digit %2 != 0){
+                length = j+1;
                 break;
             }
-
-            j--;
         }
-
-        if (j == -1) return "";
-
-        return num.substr(0,j+1);
+        
+        if(length == 0) return "";
+        return num.substr(0,length);
     }
 };
