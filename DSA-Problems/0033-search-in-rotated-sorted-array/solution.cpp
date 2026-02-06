f1cd32f598a0f5414a351eb3019e21a1b93fdd33
Abhinav Gupta <abhinavg01441@gmail.com>
2026-02-06 08:03:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.2 MB (34.61%)

--- DSA-Problems/0033-search-in-rotated-sorted-array/solution.cpp
+++ DSA-Problems/0033-search-in-rotated-sorted-array/solution.cpp
@@ -2,37 +2,43 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
 
-        int st = 0;
+        int n = nums.size();
+        int st = 0, end = n-1;
 
-        int end = nums.size()-1;
+        while (st <= end) {
 
-        while(st<=end){
+            int mid = st + (end - st) / 2;
 
-            int mid = st+(end-st)/2;
-            if(nums[mid] == target){
+            if (nums[mid] == target) {
                 return mid;
             }
 
-            if(nums[st]<= nums[mid]){ // left sorted
-                if(nums[st]<= target && target <= nums[mid]){
-                    end = mid -1;
-                }else{
-                    st = mid+1;
+            // left sorted
+            if (nums[st] <= nums[mid]) {
 
+                // since it is left sorted so if the target is between st and
+                // mid go left or go right
+
+                if (nums[st] <= target && target <= nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;
                 }
 
-            }
-            else { // right sorted
-                if(nums[mid]<= target && target <= nums[end]){
-                    st = mid+1;
-                }else{
-                    end = mid-1;
+            } else {
+                // right sorted
+
+                // since it's right sorted if the target is between mid and end
+                // go right or go left
+
+                if (nums[mid] <= target && target <= nums[end]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
                 }
-
             }
-
         }
+
         return -1;
-        
     }
 };
