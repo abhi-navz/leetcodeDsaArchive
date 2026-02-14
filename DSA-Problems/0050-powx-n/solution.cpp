9ce558b90dce1797618eab8bdab67cac8060a106
Abhinav Gupta <abhinavg01441@gmail.com>
2026-02-14 13:57:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (13.98%)

--- DSA-Problems/0050-powx-n/solution.cpp
+++ DSA-Problems/0050-powx-n/solution.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+    double power(double x, long long n ){
+        // base case 
+        if(n == 0){
+            return 1.0;
+        }
+        double half = power(x,n/2);
+        if(n%2 == 0){
+            return half*half;
+        }else{
+            return half*half*x;
+        }
+    }
     double myPow(double x, int n) {
 
+        long long N = n;
 
-        double ans = 1.0;
-        long long nn = n;
-        if(nn<0) nn = -1*nn;
-        while(nn>0){
-            if(nn%2 == 1){
-                ans *= x;
-                nn = nn-1;
-            }else{
-                x = x*x;
-                nn = nn/2;
-            }
+        if(N<0){
+
+            N = -N;
+            x = 1/x;
         }
 
-        if(n<0){
-            ans = 1.0/ans;
-        }
-
-        return ans;
-
+        return power(x,N);
 
         
     }
