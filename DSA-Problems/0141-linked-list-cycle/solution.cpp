905b534ee606523af43f7f114165b1669fb6fc94
Abhinav Gupta <abhinavg01441@gmail.com>
2026-02-17 07:47:14 +0000

    Sync LeetCode submission Runtime - 12 ms (43.43%), Memory - 11.9 MB (52.15%)

--- DSA-Problems/0141-linked-list-cycle/solution.cpp
+++ DSA-Problems/0141-linked-list-cycle/solution.cpp
@@ -9,18 +9,16 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-
-        ListNode* fast = head;
-        ListNode* slow = head;
-
-        while(fast!= nullptr && fast->next != nullptr){
+        ListNode *slow = head, *fast = head;
+        while(fast&& fast->next){
             slow = slow->next;
             fast = fast->next->next;
-            if(fast == slow) return true;
+
+            if(slow == fast){
+                return true;
+            }
         }
-
         return false;
-
         
     }
 };
