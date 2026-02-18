2a58043aeb7fb56bf322c3036875e0a4e465fcd6
Abhinav Gupta <abhinavg01441@gmail.com>
2026-02-18 15:58:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.8 MB (47.07%)

--- DSA-Problems/0100-same-tree/solution.cpp
+++ DSA-Problems/0100-same-tree/solution.cpp
@@ -11,21 +11,16 @@
  */
 class Solution {
 public:
-
-
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // if both are root are null return true.
-        if(!p && !q) return true;
+        if(!p && !q)return true;
 
-        // if one root is null but not null
-        if((!p && q) || (p&&!q)) return false;
 
-        // if none is null
-        return (p->val == q->val) && (isSameTree(p->left, q->left)) && (isSameTree(p->right,q->right));
+        if((p && !q) || (!p && q))return false;
+
+        if(p->val != q->val)return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
         
-      
-
-
+        
 
         
     }
