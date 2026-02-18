7e6a5dfb71b1046499ee9307118fe391dde6a5ac
Abhinav Gupta <abhinavg01441@gmail.com>
2026-02-18 16:49:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15 MB (77.60%)

--- DSA-Problems/0103-binary-tree-zigzag-level-order-traversal/solution.cpp
+++ DSA-Problems/0103-binary-tree-zigzag-level-order-traversal/solution.cpp
@@ -6,48 +6,46 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-         vector<vector<int>> ans;
-        if(!root) return ans;
+        vector<vector<int>> ans;
+
+        if (!root)
+            return ans;
 
         queue<TreeNode*> q;
+        bool l2r = true;
         q.push(root);
-        bool leftToRight = true;
 
-        while(!q.empty()){
+        while (!q.empty()) {
+            vector<int> lvl;
             int size = q.size();
-            vector<int> level(size); // intialising with size: so we insert at correct index
-            for(int i =0; i<size; i++){
+            for (int i = 0; i < size; i++) {
 
-                TreeNode* curr = q.front();
+                TreeNode* node = q.front();
                 q.pop();
-                
-                int index;
-                if(leftToRight){
-                  index = i;
-                }else{
-                    index = size-i-1;
+                lvl.push_back(node->val);
+                if(node->left){
+                    q.push(node->left);
+                }if(node->right){
+                    q.push(node->right);
                 }
-                level[index] = curr->val;
-                if(curr->left){
-                    q.push(curr->left);
-                }
-                if(curr->right){
-                    q.push(curr->right);
-                }
+
+               
             }
-            leftToRight = !leftToRight;
-
-            ans.push_back(level);
-
+            if(!l2r){
+                reverse(lvl.begin(),lvl.end());
+            }
+            ans.push_back(lvl);
+            l2r = !l2r;
+            
         }
 
         return ans;
-        
     }
 };
