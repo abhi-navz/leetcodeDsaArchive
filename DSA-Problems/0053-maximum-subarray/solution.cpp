0c183db8ec76faa31be133bfeb0f2ab628d791b2
Abhinav Gupta <abhinavg01441@gmail.com>
2026-02-21 16:52:02 +0000

    Sync LeetCode submission Runtime - 3 ms (19.53%), Memory - 71.8 MB (51.91%)

--- DSA-Problems/0053-maximum-subarray/solution.cpp
+++ DSA-Problems/0053-maximum-subarray/solution.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int currSum=0;
-        int maxSum = INT_MIN;
 
-        for(int i=0; i<nums.size(); i++){
-            currSum+= nums[i];
-            maxSum = max(currSum, maxSum);
+        int currSum = nums[0];
+        int maxSum = nums[0];
+        int n = nums.size();
 
-            if(currSum<0){
-                currSum=0;
-            }
-
+        for(int i =1; i<n;i++){
+            
+           
+            currSum = max(nums[i], currSum+nums[i]);
+            maxSum = max(currSum,maxSum);
+            
+            
         }
-        
+
         return maxSum;
+        
     }
 };
