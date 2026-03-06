6db1b4f756fd1714673fe5612d6bea273ff8618d
Abhinav Gupta <abhinavg01441@gmail.com>
2026-03-06 15:20:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (92.50%)

---
+++ DSA-Problems/2323-minimum-bit-flips-to-convert-number/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+
+        int n = start ^ goal;
+
+        // JUST COUNT SET BTIS OF N
+        int count = 0;
+        while (n != 0) {
+
+            count++;
+            n = n & (n - 1);
+        }
+
+        return count;
+    }
+};
