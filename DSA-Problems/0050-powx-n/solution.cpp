310756ef8150a96be77c19b141e8d9d6bec7ddac
Abhinav Gupta <abhinavg01441@gmail.com>
2026-03-12 20:23:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (43.83%)

--- DSA-Problems/0050-powx-n/solution.cpp
+++ DSA-Problems/0050-powx-n/solution.cpp
@@ -5,8 +5,8 @@ public:
         if(n == 0){
             return 1.0;
         }
-        double half = power(x,n/2);
-        if(n%2 == 0){
+        double half = power(x,n >> 1);
+        if((n&1 )== 0){
             return half*half;
         }else{
             return half*half*x;
