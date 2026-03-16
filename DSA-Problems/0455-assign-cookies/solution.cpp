19738d26e4ab374910a8a9e8593d17c6e811020a
Abhinav Gupta <abhinavg01441@gmail.com>
2026-03-16 10:19:30 +0000

    Sync LeetCode submission Runtime - 8 ms (58.79%), Memory - 44.9 MB (47.16%)

---
+++ DSA-Problems/0455-assign-cookies/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+
+        int i=0,j=0;
+        int m = g.size();
+        int n = s.size();
+
+        sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
+        int count =0;
+
+        while(i<m && j<n){
+            if(g[i]<= s[j]){
+                count++;
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+
+        return count;
+        
+    }
+};
