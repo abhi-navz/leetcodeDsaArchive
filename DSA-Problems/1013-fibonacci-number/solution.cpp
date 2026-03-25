8feb910fe847b38cccff5e23f2007a83a6edd12c
Abhinav Gupta <abhinavg01441@gmail.com>
2026-03-25 16:12:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (37.60%)

--- DSA-Problems/1013-fibonacci-number/solution.cpp
+++ DSA-Problems/1013-fibonacci-number/solution.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
+    int fib(int n) {
+       
+        if(n<= 1)return n;
 
-    int fib_helper(int n , vector<int> &memo){
-        if(n<2) return n;
+        int prev2 = 0;
+        int prev1 = 1;
 
-        // if the memo vector has already stored the value for n , return it from there directly
-        if(memo[n] != -1){
-            return memo[n];
+        
+
+        for(int i = 2; i<=n; i++){
+           
+            int curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        // otherwise calculate it and store it
+        return prev1;
 
-        memo[n] = fib_helper(n-1, memo) + fib_helper(n-2, memo);
-        return memo[n] ;
-    }
-
-
-
-    int fib(int n) {
-
-        // we'll use a vector for memoization , a vector of size n+1 would be  enough to store all the indexes from 0 to n .This will help in avoiding the fucntion call for lower values multiple times
-
-        vector<int> memo (n+1, -1);
-
-        return fib_helper(n, memo);
-    
     }
 };
