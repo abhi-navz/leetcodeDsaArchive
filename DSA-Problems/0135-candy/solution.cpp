4b1086ba3e947e5f6b46b64610b7cace15420094
Abhinav Gupta <abhinavg01441@gmail.com>
2026-03-25 01:19:52 +0000

    Sync LeetCode submission Runtime - 4 ms (14.57%), Memory - 22.2 MB (84.54%)

---
+++ DSA-Problems/0135-candy/solution.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+
+        // using concept of slope
+        int n = ratings.size();
+        int i = 1;
+        int sum = 1;
+
+        while(i<n){
+
+            if(ratings[i] == ratings[i-1]){
+                sum+=1;
+                i++;
+                continue;
+            }
+
+            int peak = 1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                sum+= peak;
+                i++;
+            }
+
+            int down = 1;
+            while(i<n && ratings[i]< ratings[i-1]){
+                sum += down;
+                down++;
+                i++;
+            }
+
+            if(down>peak){
+                sum += (down-peak);
+            }
+        }
+
+        return sum;
+        
+    }
+};
