cd1608300ac232e73b4e5cb92f26f2399fa15aed
Abhinav Gupta <abhinavg01441@gmail.com>
2026-03-26 15:33:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (17.17%)

---
+++ DSA-Problems/0070-climbing-stairs/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int helper(int n, vector<int>&dp){
+        // base cases;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = helper(n-1,dp) + helper(n-2,dp);
+    }
+    int climbStairs(int n) {
+       
+       // memoization 
+       vector<int>dp(n+1,-1);
+        return helper(n,dp);
+    }
+};
