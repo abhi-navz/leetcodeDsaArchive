ab2b471e8e0df0ac44f0894f6489c58c7f7358ec
Abhinav Gupta <abhinavg01441@gmail.com>
2026-03-29 04:06:32 +0000

    Sync LeetCode submission Runtime - 4 ms (4.28%), Memory - 11.8 MB (68.93%)

---
+++ DSA-Problems/0063-unique-paths-ii/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        if(obstacleGrid[m-1][n-1] == 1) return 0;
+        if(obstacleGrid[0][0] == 1) return 0;
+
+        vector<vector<int>>dp(m, vector<int>(n,0));
+
+        for(int i =0; i<m; i++){
+            for(int j =0; j<n; j++){
+
+                if(obstacleGrid[i][j] == 0){
+                    if(i ==0 && j == 0) dp[i][j] = 1;
+                    else{
+                        int left =0, up = 0;
+                        if(i>0) up = dp[i-1][j];
+                        if(j>0) left = dp[i][j-1];
+                        dp[i][j] = up+left;
+                    }
+                }
+
+            }
+        }
+
+        return dp[m-1][n-1];
+        
+    }
+};
