17a84baf6da617a84c29b61c257fa3c317f6c5c6
Abhinav Gupta <abhinavg01441@gmail.com>
2026-04-04 21:50:42 +0000

    Sync LeetCode submission Runtime - 15 ms (51.61%), Memory - 25.8 MB (26.13%)

--- DSA-Problems/0494-target-sum/solution.cpp
+++ DSA-Problems/0494-target-sum/solution.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
+    int n,sum;
+
+    int helper(int idx, int rem, vector<int>&arr,vector<vector<int>>&dp){
+
+        if(rem < -sum || rem > sum) return 0;
+        
+        if(idx == n){
+            if(rem == 0) return 1;
+            else return 0;
+        }
+        
+
+        if(dp[idx][rem+sum] != -1) return dp[idx][rem+sum];
+
+        int plus = helper(idx+1, rem -arr[idx], arr,dp);
+        
+        int minus = helper(idx+1, rem+arr[idx], arr,dp);
+
+       
+        return dp[idx][rem+sum] = (plus+minus);
+    }
+
     int findTargetSumWays(vector<int>& nums, int target) {
+        n = nums.size();
 
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-
-        if(abs(target) > sum) return 0;
-        if((sum + target) % 2 != 0) return 0;
-
-        int subsetSum = (sum + target) / 2;
-
-        vector<int> dp(subsetSum + 1, 0);
-        dp[0] = 1;
-
-        for(int num : nums){
-            for(int j = subsetSum; j >= num; j--){
-                dp[j] += dp[j - num];
-            }
+        sum = 0;
+        for(int x: nums){
+            sum += x;
         }
 
-        return dp[subsetSum];
+        vector<vector<int>>dp(n,vector<int>(2*sum+1,-1));
+
+        return helper(0,target, nums,dp);
+
+        
     }
 };
