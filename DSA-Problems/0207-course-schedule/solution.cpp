ad5ed2cc5315e31a666a945775c146c61d79ea8d
Abhinav Gupta <abhinavg01441@gmail.com>
2026-04-14 03:33:03 +0000

    Sync LeetCode submission Runtime - 4 ms (64.30%), Memory - 19.9 MB (39.19%)

--- DSA-Problems/0207-course-schedule/solution.cpp
+++ DSA-Problems/0207-course-schedule/solution.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
+    int topoSort(int V, vector<int>&indegree, vector<vector<int>>&adj){
+      queue<int>q;
+      int sz=0;
+      
+      for(int i =0; i<V; i++){
+          if(indegree[i] == 0){
+              q.push(i);
+            }
+        }
+       
+       
+       while(!q.empty()){
+           int node = q.front();
+           q.pop();
+           sz++;
+           
+           for(int adjNode :adj[node]){
+               indegree[adjNode]--;
+               if(indegree[adjNode] == 0){
+                   q.push(adjNode);
+               }
+           }
+       }
+       
+       return sz;
+    }
+
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
-        // making adjacency list and indegree array 
-
+        
         vector<vector<int>>adj(numCourses);
-        vector<int>indegree(numCourses);
+        vector<int>indegree(numCourses,0);
 
-        for(auto edge: prerequisites){
-            int u = edge[1];
-            int v = edge[0];
+        for(auto course : prerequisites){
+            int u = course[1];
+            int v = course[0];
 
             adj[u].push_back(v);
             indegree[v]++;
         }
 
-        vector<int>topo;
-        queue<int>q;
-
-        for(int i=0; i<numCourses; i++){
-            if(indegree[i] ==0){
-                q.push(i);
-            }
-        }
-
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-            for(int v : adj[node]){
-                indegree[v]--;
-                if(indegree[v]==0){
-                    q.push(v);
-                }
-            }
-        }
-
-        return topo.size() == numCourses ? true: false;
-
-
-        
+        int topoSize = topoSort(numCourses, indegree, adj);
+        return (topoSize == numCourses);
     }
 };
