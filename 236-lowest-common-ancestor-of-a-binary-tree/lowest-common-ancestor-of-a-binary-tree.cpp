7f10cc99dd0ef0f9e9e782f927e77a160028c8ea
Abhinav Gupta <abhinavg01441@gmail.com>
2026-07-19 05:52:02 +0530

    Time: 100 ms (5.02%) | Memory: 60.9 MB (5.17%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,51 +9,49 @@
  */
 class Solution {
 public:
-    void findParent(TreeNode* root, unordered_map<TreeNode*, TreeNode*>&parent){
-        if(!root) return;
+    void findParent(TreeNode* root,
+                    unordered_map<TreeNode*, TreeNode*>& parent) {
+        if (!root)
+            return;
 
-        if(root->left){
+        if (root->left) {
             parent[root->left] = root;
             findParent(root->left, parent);
         }
-        if(root->right){
+        if (root->right) {
             parent[root->right] = root;
             findParent(root->right, parent);
         }
-
-
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         TreeNode* ans = nullptr;
-        unordered_map<TreeNode*, TreeNode*>parent;
+        unordered_map<TreeNode*, TreeNode*> parent;
         parent[root] = root;
 
         findParent(root, parent);
 
-        unordered_set<TreeNode*>visited; // to track visisted
+        unordered_set<TreeNode*> visited; // to track visisted
         // visiting from p to root first an storing in set.
 
         visited.insert(p);
         TreeNode* node = p;
-        while(parent[node] != node){
+        while (parent[node] != node) {
             node = parent[node];
             visited.insert(node);
         }
 
-        // visit from q to root and if there's already a node in the set, it's lca.
+        // visit from q to root and if there's already a node in the set, it's
+        // lca.
 
         node = q;
 
-        while(node != parent[node]){
-            if(visited.find(node) == visited.end()){
-                node = parent[node];
-            }else{
+        while (node != parent[node]) {
+            if (visited.count(node)) {
                 return node;
-            }
+            } 
+            node = parent[node];
         }
 
-        return root;
-
-
+        return node; // root;
     }
 };
