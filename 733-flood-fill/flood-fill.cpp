c7ae99f810cc502a6709297c78187d9ff1b3722a
Abhinav Gupta <abhinavg01441@gmail.com>
2026-07-26 06:51:49 +0530

    Time: 4 ms (5.52%) | Memory: 17.9 MB (90.69%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        
-        int m = image.size();
-        int n = image[0].size();
+    int dir[4][2] = {{-1,0},{1,0},{0,1},{0,-1}};
+    void dfs(vector<vector<int>>&img,int row, int col, int oc, int color){
+        int m = img.size();
+        int n = img[0].size();
+        img[row][col] = color;
+        for(auto &d : dir){
+            int nr = row+d[0];
+            int nc = col +d[1];
 
-        int oc = image[sr][sc];
-        if(oc == color) return image;
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        image[sr][sc] = color;
-
-        int dir[4][2] ={{-1,0}, {1,0}, {0,-1},{0,1}};
-
-        while(!q.empty()){
-            auto[row,col] = q.front();
-            q.pop();
-
-            for(auto &d : dir){
-                int nr = row+d[0];
-                int nc = col+d[1];
-
-                if(nr>=0 && nr<m && nc>=0 && nc<n && image[nr][nc] == oc){
-                    image[nr][nc] = color;
-                    q.push({nr,nc});
-                }
+            if(nr>=0 && nr<m && nc>=0 && nc<n && img[nr][nc] == oc){
+                dfs(img,nr,nc,oc,color);
             }
         }
 
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        
+        int oc = image[sr][sc];
+        if(oc == color) return image;
+
+        dfs(image,sr,sc,oc,color);
         return image;
     }
 };
