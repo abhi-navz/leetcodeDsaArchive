62a5180442ead50f3cbc8aa2b510ac995988c964
Abhinav Gupta <abhinavg01441@gmail.com>
2026-07-27 18:43:10 +0530

    Time: 3 ms (94.35%) | Memory: 52.5 MB (79.45%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     
-    bool dfs(int node, vector<vector<int>>&adj, vector<int>&vis, vector<int>&path, vector<int>&safe){
+    bool dfs(int node, vector<vector<int>>&adj, vector<int>&vis, vector<int>&path){
 
         path[node] = 1;
-        vis[node] = 1;
+        vis[node] = 0;
 
         for(int next:adj[node]){
-            if(!vis[next]){
-                if(!dfs(next,adj,vis,path,safe)) return false;
+            if(vis[next] == -1){
+                if(!dfs(next,adj,vis,path)) return false;
             }else{
                 if(path[next]){
                     return false;
@@ -16,8 +16,8 @@ public:
             }
             
         }
-        safe[node] = 1;
         path[node] = 0;
+        vis[node] = 1;
         return true;
 
     }
@@ -25,18 +25,18 @@ public:
         int V = graph.size();
         vector<int>ans;
 
-        vector<int>vis(V,0);
+        vector<int>vis(V,-1); // {-1: not visited, 0: visited , 1: visited and marked safe}
         vector<int>path(V,0);
-        vector<int>safe(V,0);
+        
 
         for(int i=0;i<V; i++){
-            if(!vis[i]){
-               dfs(i,graph,vis,path,safe);
+            if(vis[i] == -1){
+               dfs(i,graph,vis,path);
             }
         }
 
         for(int i=0; i<V; i++){
-            if(safe[i] == 1) ans.push_back(i);
+            if(vis[i] == 1) ans.push_back(i);
         }
 
         return ans;
