07c7efca67a606a1b9559f1aa7270cb901e4f0ef
Abhinav Gupta <abhinavg01441@gmail.com>
2026-07-27 13:45:39 +0530

    Time: 5 ms (85.50%) | Memory: 52.5 MB (79.45%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -10,10 +10,13 @@ public:
                     return true;
                 }
             }else{
-                if(path[adjNode] == 1)return true;
+                if(path[adjNode] == 1)
+                    return true;
             }
         }
-        path[node] = 0;
+
+        path[node] = 0; // exiting the node from path 
+
         return false;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
@@ -24,12 +27,10 @@ public:
         // the nodes which are not safe will eventually go into cycle so i'll
         // have to check which nodes deoesn't form a cycle.
 
-        
+        vector<int> vis(V, 0);
+        vector<int> path(V, 0);
 
         for (int i = 0; i < V; i++) {
-
-            vector<int> vis(V, 0);
-            vector<int> path(V, 0);
 
             if (!dfs(i, vis, path, graph)) {
                 ans.push_back({i});
