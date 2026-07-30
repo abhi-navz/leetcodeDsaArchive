a499040c54dca0713b77bb3f41b90a336aa77913
Abhinav Gupta <abhinavg01441@gmail.com>
2026-07-31 04:07:51 +0530

    Time: 4 ms (79.43%) | Memory: 42.2 MB (72.40%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,49 +1,62 @@
+class DisjointSetUnion {
+	private:
+	vector<int>parent, rank;
+	public:
+	DisjointSetUnion(int n) {
+		parent.resize(n + 1);
+		rank.resize(n + 1, 0);
+		
+		for (int i = 0; i <= n; i++) {
+			parent[i] = i;
+		}
+	}
+	
+	int findUP(int node) {
+		if (parent[node] == node) {
+			return node;
+		}
+		return parent[node] = findUP(parent[node]);
+	}
+	
+	void unionByRank(int u, int v) {
+		int pu = findUP(u);
+		int pv = findUP(v);
+		
+		// if (pu == pv) return; // same component
+		
+		if (rank[pu]< rank[pv]) {
+			parent[pu] = pv;
+		} else if (rank[pv] < rank[pu]) {
+			parent[pv] = pu;
+		} else {
+			parent[pu] = pv;
+			rank[pv]++;
+		}
+		
+	}
+};
+
 class Solution {
 public:
-    void bfs(int node, vector<int>&vis, const vector<vector<int>>&adj){
-        queue<int>q;
-        q.push(node);
-        vis[node] = 1;
-
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            for(int nxt : adj[curr]){
-                if(!vis[nxt]){
-                    vis[nxt] = 1;
-                    q.push(nxt);
-                }
-            }
-        }
-    }
-
     int makeConnected(int n, vector<vector<int>>& connections) {
-        
-        int cabels = connections.size();
-        if(cabels < n-1) return -1;
 
-        vector<vector<int>>adj(n);
-        for(const auto &cable : connections){
+        int cables = connections.size();
+        if(cables < n-1) return -1;
 
-            int u = cable[0];
-            int v  = cable[1];
+        DisjointSetUnion ds(n);
+        int components = n;
 
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
+        for(const auto &connection : connections){
+            int u = connection[0];
+            int v = connection[1];
 
-        vector<int>vis(n,0);
-
-        int components = 0;
-        for(int i =0; i<n; i++){
-            if(!vis[i]){
-                components++;
-                bfs(i, vis,adj);
+            if(ds.findUP(u) != ds.findUP(v)){
+                ds.unionByRank(u,v);
+                components--;
             }
         }
-        
 
-        return components -1;
+        return components-1;
 
 
     }
