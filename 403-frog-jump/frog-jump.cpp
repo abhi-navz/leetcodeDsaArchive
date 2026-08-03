b6eb99ce4cca946d88ceb7a1f46025859610ecf2
Abhinav Gupta <abhinavg01441@gmail.com>
2026-08-03 14:07:31 +0530

    Time: 193 ms (17.63%) | Memory: 239.8 MB (33.14%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -36,7 +36,29 @@ public:
         if (stones[1] != 1)
             return false;
 
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return helper(1, 1, stones, dp);
+        vector<vector<int>> dp(n, vector<int>(n + 1, 0));
+
+        dp[1][1] = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n + 1; j++) {
+
+                if (dp[i][j] == 1) {
+                    for (int a = -1; a <= 1; a++) {
+                        int jump = j + a;
+                        if (jump <= 0)
+                            continue;
+                        int np = stones[i] + jump;
+                        if (pos.count(np)) {
+                            dp[pos[np]][jump] = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        for(int j = 0; j<n+1; j++){
+            if(dp[n-1][j] == 1) return true;
+        }
+        return false;
     }
 };
