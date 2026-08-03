60c9f38d1fa1ead81f0ec474baac67f962d6ef68
Abhinav Gupta <abhinavg01441@gmail.com>
2026-08-03 14:07:41 +0530

    Time: 188 ms (18.18%) | Memory: 239.7 MB (35.74%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -3,30 +3,7 @@ class Solution {
     unordered_map<int, int> pos;
 
 public:
-    bool helper(int idx, int k, vector<int>& stones, vector<vector<int>>& dp) {
-        int n = stones.size();
-        if (idx == n - 1)
-            return true;
-
-        if (dp[idx][k] != -1) {
-            return dp[idx][k]; // {0,1} false or true
-        }
-
-        for (int j = -1; j <= 1; j++) {
-            int jump = k + j;
-            if (jump <= 0)
-                continue;
-
-            int nextPos = stones[idx] + jump;
-            if (pos.count(nextPos)) {
-                if (helper(pos[nextPos], jump, stones, dp))
-                    return dp[idx][k] = true;
-            }
-        }
-
-        return dp[idx][k] = false;
-    }
-
+   
     bool canCross(vector<int>& stones) {
         int n = stones.size();
         for (int i = 0; i < n; i++) {
