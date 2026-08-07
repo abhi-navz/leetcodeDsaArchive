09a328e043ad708ece86380b0a9efa987817f8c5
Abhinav Gupta <abhinavg01441@gmail.com>
2026-08-07 23:12:49 +0530

    Time: 1039 ms (24.94%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -4,9 +4,9 @@ from  Employee e1 join Department d
 on e1.departmentId = d.id
 where e1.salary = (
     select max(salary) 
-    from employee e2 join department d 
-    on e2.departmentId = d.id 
-    where e2.departmentId = e1.departmentId
-    group by d.name
+    from employee e2 
+    where e2.departmentId = e1.departmentId  -- already restricts the department to just one dept
 );
 
+
+-- this is correlated subquery where the subquery runs for each row of outer query 
