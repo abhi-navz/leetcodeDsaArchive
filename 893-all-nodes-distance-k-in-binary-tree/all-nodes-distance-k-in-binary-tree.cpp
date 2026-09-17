fe09c59b250ad3549d8443a6297697c1d128cb4b
Abhinav Gupta <abhinavg01441@gmail.com>
2026-09-17 17:44:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (83.47%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -9,47 +9,56 @@
  */
 class Solution {
 public:
+
     void markParent(TreeNode* root, unordered_map<TreeNode*, TreeNode*>&parent){
+
         if(!root) return;
         if(root->left){
             parent[root->left] = root;
             markParent(root->left, parent);
         }
+
         if(root->right){
             parent[root->right] = root;
             markParent(root->right, parent);
         }
+
     }
 
-    void findNodesAtK(TreeNode* source, TreeNode* last, int k,vector<int>&ans, unordered_map<TreeNode*, TreeNode*>&parent){
 
-        if(k == 0){
+    void getNodes(TreeNode* source, TreeNode* last, vector<int>&ans, int k, unordered_map<TreeNode*, TreeNode*>&parent){
+
+
+        if(k ==0){
             ans.push_back(source->val);
-            return ;
+            return;
         }
 
         if(source->left && source->left != last){
-            findNodesAtK(source->left, source, k-1,ans,parent);
+            getNodes(source->left, source, ans, k-1, parent);
         }
 
         if(source->right && source->right != last){
-            findNodesAtK(source->right, source, k-1,ans,parent);
+            getNodes(source->right, source,ans, k-1, parent);
         }
-        if(parent[source] && parent[source] != last){
-            findNodesAtK(parent[source], source, k-1, ans, parent);
+
+        if(parent.count(source) && parent[source] != last){
+            getNodes(parent[source], source, ans, k-1, parent);
         }
+
 
     }
 
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        
         vector<int>ans;
-        unordered_map<TreeNode*, TreeNode*>parent;
-        parent[root] = nullptr;
+        if(!root) return  ans;
+
+        unordered_map<TreeNode*, TreeNode*> parent;
         markParent(root, parent);
 
-        findNodesAtK(target, nullptr, k, ans,parent);
+        getNodes(target,nullptr,ans,k, parent);
         return ans;
+
 
 
 
